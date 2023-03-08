d58d87f07e1cb39f995d63f960b7196e952cff8e
arivusurya <arivusurya.3@gmail.com>
2023-03-08 20:15:44 +0530

    adding duplicate

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Day23Sets
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            AddressBook myAddressBook = new AddressBook();
+            Contacts Contact1 = new Contacts("ares","zeus","olympus","12345678","areszeus@godofgreek.com");
+            Contacts Contact2 = new Contacts("athena","Zeus","olympus","12344545","athena@godofgreek.com");
+            Contacts Contact3 = new Contacts("Loki","odinson","Asgard","987654321","Loki@godofasgrad.com");
+
+            myAddressBook.AddContact(Contact1);
+            myAddressBook.AddContact(Contact2);
+            myAddressBook.AddContact(Contact3);
+         
+        }
+    }
+}
