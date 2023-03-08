04ca836f36284bff7620ab528b39d37cbe61b809
arivusurya <arivusurya.3@gmail.com>
2023-03-08 20:19:33 +0530

    search contact with country

--- Program.cs
+++ Program.cs
@@ -7,11 +7,16 @@
             AddressBook myAddressBook = new AddressBook();
             Contacts Contact1 = new Contacts("ares","zeus","olympus","12345678","areszeus@godofgreek.com");
             Contacts Contact2 = new Contacts("athena","Zeus","olympus","12344545","athena@godofgreek.com");
-            Contacts Contact3 = new Contacts("Loki","odinson","Asgard","987654321","Loki@godofasgrad.com");
+            Contacts Contact3 = new Contacts(firstName: "Loki","odinson","Asgard","987654321","Loki@godofasgrad.com");
+            Contacts Contact4 = new Contacts(firstName: "Loki","odinson","Asgard","987654321","Loki@godofasgrad.com");
 
             myAddressBook.AddContact(Contact1);
             myAddressBook.AddContact(Contact2);
             myAddressBook.AddContact(Contact3);
+            myAddressBook.AddContact(Contact4);
+
+             myAddressBook.SearchByCountry("olympus");
+
          
         }
     }
